51b8d8f616c204ab11e84ec132fffbf73ca90c76
Pierce13 <piercescott@weber.edu>
2024-06-01 08:40:25 -0600

    modified ctests

--- src/SimpleCalculatorTest.cpp
+++ src/SimpleCalculatorTest.cpp
@@ -22,76 +22,101 @@ bool checkTest(int testNum, int& correct, T whatItShouldBe, T whatItIs) {
     }
 }
 
-// Function to test the SimpleCalculator program
-void testSimpleCalculator() {
+// Function to test calculateCircleArea()
+void testCalculateCircleArea(int& correctTests) {
+    string circleAreaInput = "5\n";
+    stringstream inputBuffer(circleAreaInput);
+    streambuf* oldCin = cin.rdbuf(inputBuffer.rdbuf());
+
+    stringstream outputBuffer;
+    streambuf* oldCout = cout.rdbuf(outputBuffer.rdbuf());
+
+    double areaResult = calculateCircleArea();
+
+    cin.rdbuf(oldCin);
+    cout.rdbuf(oldCout);
+
+    // Extract the actual result from the output buffer
+    stringstream ss;
+    ss << fixed << setprecision(10) << areaResult;
+    string capturedOutput = ss.str();
+    string expectedOutput = "78.5398163375";
+    checkTest(1, correctTests, expectedOutput, capturedOutput);
+}
+
+// Function to test addFractions()
+void testAddFractions(int& correctTests) {
+    string addFractionsInput = "1/2 1/3\n";
+    stringstream inputBuffer(addFractionsInput);
+    streambuf* oldCin = cin.rdbuf(inputBuffer.rdbuf());
+
+    stringstream outputBuffer;
+    streambuf* oldCout = cout.rdbuf(outputBuffer.rdbuf());
+
+    string capturedOutput2 = addFractions();
+
+    cin.rdbuf(oldCin);
+    cout.rdbuf(oldCout);
+
+    string expectedOutput2 = "5/6";
+    checkTest(2, correctTests, expectedOutput2, capturedOutput2);
+}
+
+// Function to test solveQuadraticFormula()
+void testSolveQuadraticFormula(int& correctTests) {
+    string solveQuadraticInput = "1 2 1\n";
+    stringstream inputBuffer(solveQuadraticInput);
+    streambuf* oldCin = cin.rdbuf(inputBuffer.rdbuf());
+
+    stringstream outputBuffer;
+    streambuf* oldCout = cout.rdbuf(outputBuffer.rdbuf());
+
+    string capturedOutput3 = solveQuadraticFormula();
+
+    cin.rdbuf(oldCin);
+    cout.rdbuf(oldCout);
+
+    string expectedOutput3 = "-1.000000, -1.000000";
+    checkTest(3, correctTests, expectedOutput3, capturedOutput3);
+}
+
+// Function to run all tests
+void runAllTests() {
     int correctTests = 0;
-
-    // Test calculateCircleArea()
-    {
-        string circleAreaInput = "5\n";
-        stringstream inputBuffer(circleAreaInput);
-        streambuf* oldCin = cin.rdbuf(inputBuffer.rdbuf());
-
-        stringstream outputBuffer;
-        streambuf* oldCout = cout.rdbuf(outputBuffer.rdbuf());
-
-        double areaResult = calculateCircleArea();
-
-        cin.rdbuf(oldCin);
-        cout.rdbuf(oldCout);
-
-        // Extract the actual result from the output buffer
-        stringstream ss;
-        ss << fixed << setprecision(10) << areaResult;
-        string capturedOutput = ss.str();
-        string expectedOutput = "78.5398163375";
-        checkTest(1, correctTests, expectedOutput, capturedOutput);
-    }
-
-
-    // Test addFractions()
-    {
-        string addFractionsInput = "1/2 1/3\n";
-        stringstream inputBuffer(addFractionsInput);
-        streambuf* oldCin = cin.rdbuf(inputBuffer.rdbuf());
-
-        stringstream outputBuffer;
-        streambuf* oldCout = cout.rdbuf(outputBuffer.rdbuf());
-
-        string capturedOutput2 = addFractions();
-
-        cin.rdbuf(oldCin);
-        cout.rdbuf(oldCout);
-
-        string expectedOutput2 = "5/6";
-        checkTest(2, correctTests, expectedOutput2, capturedOutput2);
-    }
-
-    // Test solveQuadraticFormula()
-    {
-        string solveQuadraticInput = "1 2 1\n";
-        stringstream inputBuffer(solveQuadraticInput);
-        streambuf* oldCin = cin.rdbuf(inputBuffer.rdbuf());
-
-        stringstream outputBuffer;
-        streambuf* oldCout = cout.rdbuf(outputBuffer.rdbuf());
-
-        string capturedOutput3 = solveQuadraticFormula();
-
-        cin.rdbuf(oldCin);
-        cout.rdbuf(oldCout);
-
-        string expectedOutput3 = "-1.000000, -1.000000";
-        checkTest(3, correctTests, expectedOutput3, capturedOutput3);
-    }
-
-    // Print total tests passed
+    testCalculateCircleArea(correctTests);
+    testAddFractions(correctTests);
+    testSolveQuadraticFormula(correctTests);
     cout << "Total tests passed: " << correctTests << " out of 3" << endl;
 }
 
-#ifdef TESTING
-int main() {
-    testSimpleCalculator();
+// Main function to run specific tests
+int main(int argc, char* argv[]) {
+    if (argc == 1) {
+        runAllTests();
+    } else if (argc == 2) {
+        int correctTests = 0;
+        int testNumber = atoi(argv[1]);
+
+        switch (testNumber) {
+            case 1:
+                testCalculateCircleArea(correctTests);
+                break;
+            case 2:
+                testAddFractions(correctTests);
+                break;
+            case 3:
+                testSolveQuadraticFormula(correctTests);
+                break;
+            default:
+                cerr << "Invalid test number. Please enter 1, 2, or 3." << endl;
+                return 1;
+        }
+
+        cout << "Total tests passed: " << correctTests << " out of 1" << endl;
+    } else {
+        cerr << "Usage: " << argv[0] << " [testNumber]" << endl;
+        return 1;
+    }
+
     return 0;
 }
-#endif
