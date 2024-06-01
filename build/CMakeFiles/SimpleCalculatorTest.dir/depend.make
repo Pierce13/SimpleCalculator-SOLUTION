36942f959e03a5bc6205ce682b4391ff3abda29c
Pierce13 <piercescott@weber.edu>
2024-06-01 07:44:42 -0600

    added valgrind files

--- build/CMakeFiles/SimpleCalculatorTest.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for SimpleCalculatorTest.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/piercescott/Weber State/Fall 2024/CS 1410/Simple-Calculator-SOLUTION
-# Build directory: /Users/piercescott/Weber State/Fall 2024/CS 1410/Simple-Calculator-SOLUTION/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
